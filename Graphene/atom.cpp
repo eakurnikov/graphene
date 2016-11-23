5fe1111e70761768fd3f3db10fca4581ded63f75
Kurnikov Egor <e.kurnikov94@gmail.com>
2016-11-23 21:06:58 +0300

    Update atom.cpp

--- Graphene/atom.cpp
+++ Graphene/atom.cpp
@@ -1,33 +1,33 @@
 #include "atom.hpp"
 
-// Конструктор
+// РљРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ
 Atom::Atom(double const x, double const y, double const z, char const type)
   : m_x(x), m_y(y), m_z(z), m_type(type)
 {}
 
-// Конструктор копирования
+// РљРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ РєРѕРїРёСЂРѕРІР°РЅРёСЏ
 Atom::Atom(Atom const & atom)
   : m_x(atom.getX()), m_y(atom.getY()), m_z(atom.getZ()), m_type(atom.getType())
 {}
 
-// Сеттеры
+// РЎРµС‚С‚РµСЂС‹
 void Atom::setX(double const x) { m_x = x; };
 void Atom::setY(double const y) { m_y = y; };
 void Atom::setZ(double const z) { m_z = z; };
 void Atom::setType(char const type) { m_type = type; };
 
-// Геттеры
+// Р“РµС‚С‚РµСЂС‹
 double const & Atom::getX() const { return m_x; };
 double const & Atom::getY() const { return m_y; };
 double const & Atom::getZ() const { return m_z; };
 char const & Atom::getType() const { return m_type; };
 
-// Дополнительные методы
+// Р”РѕРїРѕР»РЅРёС‚РµР»СЊРЅС‹Рµ РјРµС‚РѕРґС‹
 void Atom::plusX(double const x) { m_x += x; };
 void Atom::plusY(double const y) { m_y += y; };
 void Atom::plusZ(double const z) { m_z += z; };
 
-// Оператор присваивания
+// РћРїРµСЂР°С‚РѕСЂ РїСЂРёСЃРІР°РёРІР°РЅРёСЏ
 Atom & Atom::operator = (Atom const & atom)
 {
   if (this == &atom) return *this;
@@ -40,7 +40,7 @@ Atom & Atom::operator = (Atom const & atom)
   return *this;
 }
 
-// Оператор вывода в поток
+// РћРїРµСЂР°С‚РѕСЂ РІС‹РІРѕРґР° РІ РїРѕС‚РѕРє
 ostream & operator << (ostream & os, Atom const & atom)
 {
   os << "Atom {" << atom.getX() << ", " << atom.getY() << ", " << atom.getZ() << ", " << atom.getType() << "}";
